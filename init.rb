ebb0c94ff028ab724d51c8b8d102ed0f413b6178
Martin Dimov <martin@dimov.cz>
2025-12-15 10:02:00 +0000

    Update for Redmine 6 compatibility
    - Add version requirement >= 6.0.0
    - Update plugin metadata for FEO fork
    - Add after_initialize hook for proper loading

--- init.rb
+++ init.rb
@@ -2,14 +2,18 @@ require 'htmlentities'
 
 Redmine::Plugin.register :redmine_email_attachments do
   name 'Redmine Email Attachments plugin'
-  author 'Jon Goldberg'
-  description 'Send attachments directly in notification emails.'
+  author 'FEO'
+  description 'Send attachments directly in notification emails. Upgraded for Redmine 6 compatibility. Original by Jon Goldberg.'
   version '0.3.0'
-  url 'http://github.com/MegaphoneJon/redmine_email_attachments'
-  author_url 'http://megaphonetech.com'
+  url 'https://github.com/feo-cz/redmine_email_attachments'
+  author_url 'https://www.feo.cz'
+  requires_redmine version_or_higher: '6.0.0'
 end
 
 # Load the patch after Rails initialization to ensure Setting and Redmine classes are available
 Rails.application.config.after_initialize do
+  Rails.logger.info "Email Attachments Plugin: Loading attachment_patch.rb"
   require_relative 'lib/attachment_patch'
+  Rails.logger.info "Email Attachments Plugin: attachment_patch.rb loaded, interceptor should be registered"
+#  Rails.logger.info "Email Attachments Plugin: Registered interceptors: #{ActionMailer::Base.mail_interceptors.inspect}"
 end
